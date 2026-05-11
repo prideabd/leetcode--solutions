8411af2b7a40711855f92da01070bded9658a97a
prideabd <cfz981130@163.com>
2026-05-11 23:54:33 +0800

    add_two_sum_solution

---
+++ 1.两数之和.cpp
@@ -0,0 +1,22 @@
+/*
+ * @lc app=leetcode.cn id=1 lang=cpp
+ *
+ * [1] 两数之和
+ */
+
+// @lc code=start
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        std::unordered_map<int, int> mp;
+        for (int i = 0; i < nums.size(); i++) {
+            if (mp.find(target - nums[i]) != mp.end()) {
+                return {mp[target - nums[i]], i};
+            }
+            mp[nums[i]] = i;
+        }
+        return {};
+    }
+};
+// @lc code=end
+
