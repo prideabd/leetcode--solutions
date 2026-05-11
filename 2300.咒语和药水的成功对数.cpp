8c4079b6e9eb819412a158553322e4cbd3c64dfb
prideabd <cfz981130@163.com>
2026-05-12 00:00:10 +0800

    add_two_sums

---
+++ 2300.咒语和药水的成功对数.cpp
@@ -0,0 +1,15 @@
+/*
+ * @lc app=leetcode.cn id=2300 lang=cpp
+ *
+ * [2300] 咒语和药水的成功对数
+ */
+
+// @lc code=start
+class Solution {
+public:
+    vector<int> successfulPairs(vector<int>& spells, vector<int>& potions, long long success) {
+        
+    }
+};
+// @lc code=end
+
