f8a34bda40daffb16b9ca89fe35f85f72abac65c
prideabd <cfz981130@163.com>
2026-05-13 20:38:01 +0800

    cmake小测试

---
+++ cmake/main.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+#include "calc.h"
+
+int main() {
+    std::cout << "1 + 1 等于 " << add(1, 1) << std::endl;
+    std::cout << "100 * 100 等于 " << mul(100, 100) << std::endl;
+    return 0;
+}
